d70c64e1fe8864679f3cac786a21370dcd479395
Zhurks01~ <Vanessa_99@mail.ru>
2022-11-30 23:06:53 +0500

    added articles section

--- app/js/main.js
+++ app/js/main.js
@@ -3,8 +3,19 @@ $(function(){
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
   });
 
-  var mixer = mixitup('.week-products__items');
-
+  var containerEl1 = document.querySelector('[data-ref="week-products"]');
+  var containerEl2 = document.querySelector('[data-ref="new-design"]');
+ 
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+ 
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
 });
